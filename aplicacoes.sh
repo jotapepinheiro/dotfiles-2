d8fa4c9a27d1ed1542c62aeec7909fb8124ae1dd
Ulisses Marcondes <contato@ulissesms.com.br>
2019-10-03 11:33:14 -0300

    ajuste na ordem de instalacao

---
+++ aplicacoes.sh
@@ -0,0 +1,48 @@
+#!/zsh
+echo "Installing NVM"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
+
+echo "Installing node"
+nvm install 12.7.0 && nvm alias default 12.7.0
+
+echo "Installing watchman"
+brew install watchman
+
+echo "Installing yarn"
+brew install yarn
+
+echo "Installing React Native CLI"
+yarn global add react-native-cli
+
+echo "Installing chrome"
+brew cask install google-chrome
+
+echo "Installing VS Code"
+brew cask install visual-studio-code
+
+echo "Installing VLC"
+brew cask install vlc
+
+echo "Installing AWS CLI"
+brew install awscli
+
+echo "Installing ffmpeg"
+brew install ffmpeg
+
+echo "Installing imagemagick"
+brew install imagemagick
+
+echo "Installing docker"
+brew cast install docker
+
+echo "Installing Font Fira-Code"
+brew tap homebrew/cask-fonts && brew cask install font-fira-code
+
+echo "Installing insomnia"
+brew cask install insomnia
+
+echo "Installing Reactotron"
+brew cask install reactotron
+
+echo "Installing MongoDB Compass"
+brew cask install mongodb-compass
--- install.sh
+++ install.sh
@@ -19,9 +19,6 @@ chsh -s $(which zsh)
 echo "Installing Oh My Zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-echo "Appending custom .zshrc tail... remember to change the theme to 'spaceship'"
-cat $(pwd)/.zshrc_tail >> ~/.zshrc
-
 echo "Installing zplugin"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
 
@@ -30,53 +27,10 @@ git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/theme
 
 echo "Simlink spaceship"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+cp .zshrc ~/.zshrc
 
-echo "Installing NVM"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
+echo "Ambiente configurado. Agora vamos instalar os apps "
 
-echo "Installing node"
-node install 10.16.3 && node alias default 10.16.3
-
-echo "Installing watchman"
-brew install watchman
-
-echo "Installing yarn"
-brew install yarn --without-node
-
-echo "Installing React Native CLI"
-yarn global add react-native-cli
-
-echo "Installing chrome"
-brew cask install google-chrome
-
-echo "Installing VS Code"
-brew cask install visual-studio-code
-
-echo "Installing VLC"
-brew cask install vlc
-
-echo "Installing AWS CLI"
-brew install awscli
-
-echo "Installing ffmpeg"
-brew install ffmpeg
-
-echo "Installing imagemagick"
-brew install imagemagick
-
-echo "Installing docker"
-brew cast install docker
-
-echo "Installing Font Fira-Code"
-brew tap homebrew/cask-fonts && brew cask install font-fira-code
-
-echo "Installing insomnia"
-brew cask install insomnia
-
-echo "Installing Reactotron"
-brew cask install reactotron
-
-echo "Installing MongoDB Compass"
-brew cask install mongodb-compass
+sh -c $(pwd)/./aplicacoes.sh
 
 echo "Done!"
